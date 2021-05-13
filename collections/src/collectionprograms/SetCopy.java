acdc9dc97d35af1053d10c325fce48ad9a8a5c76
anureddy syamakuri <anureddy syamakuri@DESKTOP-3JUCPUV>
2021-05-13 17:37:17 +0530

    collectionprograms

---
+++ collections/src/collectionprograms/SetCopy.java
@@ -0,0 +1,16 @@
+package collectionprograms;
+
+import java.util.HashSet;
+
+public class SetCopy {
+	 public static void main(String[] args) {
+		 HashSet <String> source = new HashSet <String>();
+	      HashSet <String> destination = new HashSet <String>();
+	      source.add("prasad");
+	      source.add("royal");
+	      System.out.println("Source :"+source);
+	      destination = (HashSet) source.clone();
+	      System.out.println("Destination :"+destination);
+	}
+
+}
